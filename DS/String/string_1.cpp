8677faabb1301217d671a291072d2276c8a87275
HYC <yechanhwangahah@gmail.com>
2026-04-22 10:26:25 +0900

    cstring 구현

--- DS/String/string_1.cpp
+++ DS/String/string_1.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(){
     string s = "hello";
-    cout << s;
+    cout << s << endl;
 
+    char str1[20] = "world";
+    char str2[20] = "!!!";
+
+    strcat(str1, str2);
+    cout << "결과" << str1 << str2 <<endl;
+    return 0;
 }
 
