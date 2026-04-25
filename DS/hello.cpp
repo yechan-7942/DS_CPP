09c2787f5be1f27f7cbc5242e29d65c5bad0fbec
HYC <yechanhwangahah@gmail.com>
2026-04-25 14:16:11 +0900

    숫자 더하기 완룡

--- DS/hello.cpp
+++ DS/hello.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main(){
     int n; 
+    int d;
     cout << "숫자를 입력하시오";
     cin >> n;
-    cout << "입력한 수는 : " ;
+    cout << "입력한 수는 : " << n << endl;
+    
+    d = n *n +3;
+    cout << "d는 " << d ;
 }
