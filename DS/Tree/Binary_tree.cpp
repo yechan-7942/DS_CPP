55a0d62ed93b8dda9e9443f82d38bc11b8b639c7
HYC <yechanhwangahah@gmail.com>
2026-04-26 23:28:17 +0900

    score_sum

--- DS/Tree/Binary_tree.cpp
+++ DS/Tree/Binary_tree.cpp
@@ -58,3 +58,39 @@ int my_tree::insert_left(node *p, string tname, node tnode) {
         return insert_left(p->right, tname, tnode);
     }
 }
+
+int my_tree::insert_right(node *p, string tname, node tnode) {
+    if (p == NULL) return 0; // 탐색 실패
+
+    if (p->name == tname) {
+        if (p->right != NULL) return -1; // 이미 왼쪽 자식이 있으면 실패
+
+        node *newNode = new node;
+        *newNode = tnode; // 값 복사
+        newNode->right = NULL;
+        newNode->left = NULL;
+        p-> right = newNode;
+        node_count++;
+        return 1; // 성공
+    }
+    else {
+        // 왼쪽 서브트리 탐색
+        int result = insert_left(p->right, tname, tnode);
+        if (result != 0) return result;
+
+        // 오른쪽 서브트리 탐색
+        return insert_right(p->left, tname, tnode);
+    }
+}
+
+double my_tree :: score_sum(){
+    return sum_allnodes(root);
+}
+double sum_allnodes(node *p){
+    if( p == NULL){
+        return 0;
+    }
+    return sum_allnodes(p ->left) + sum_allnodes(p->right) + p->score;
+}
+
+
