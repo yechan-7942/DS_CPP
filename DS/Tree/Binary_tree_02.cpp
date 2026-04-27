dd3a43fa8936ca1d410cf501cd812a675f74314d
HYC <yechanhwangahah@gmail.com>
2026-04-27 23:11:49 +0900

    insert_left 함수 구현

--- DS/Tree/Binary_tree_02.cpp
+++ DS/Tree/Binary_tree_02.cpp
@@ -20,8 +20,8 @@ class my_tree{
     node *root;
     my_tree();
     int insert_root(node t);
-    int insert_left(node *p , string tname , node t);
-    int insert_right(node *p , string tname , node t);
+    int insert_left(string tname , node t);
+    int insert_right( string tname , node t);
 
     double score_sum();
     double score_average();
@@ -33,4 +33,36 @@ class my_tree{
 my_tree :: my_tree(){
     node_count = 0;
     root = NULL;
+}
+
+int my_tree :: insert_left(string tname, node tnode){
+    int result;
+    result = node_insert_left(root, tname, tnode);
+    if(result ==1){
+        node_count ++;
+    }
+    return result;
+}
+
+int node_insert_left(node *p, string tname, node tnode){
+    if(p == NULL) return 0;
+    if(p-> name ==tname){
+        if(p->left != NULL){
+            return -1;
+        }
+        node *t;
+        t = new node;
+         (*t) = tnode ;
+         t->left = NULL;
+         t->right = NULL;
+         p->left = t;
+         return 1;
+    }
+    else{
+        int result = node_insert_left(p->left , tname, tnode);
+        if(result != 0){
+            return result;
+            return node_insert_left(p->right ,tname, tnode);
+        }
+    }
 }
