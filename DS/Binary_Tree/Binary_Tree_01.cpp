f5ac668212b2a43f6f384fccc3a813337d5fe720
HYC <yechanhwangahah@gmail.com>
2026-05-13 19:32:14 +0900

    insert_left

--- DS/Binary_Tree/Binary_Tree_01.cpp
+++ DS/Binary_Tree/Binary_Tree_01.cpp
@@ -20,7 +20,7 @@ class my_tree{
     node *root;
     my_tree();
     int insert_root(node t);
-
+    int insert_left(string tname, node tnode);
 };
 
 my_tree :: my_tree(){
@@ -28,6 +28,57 @@ my_tree :: my_tree(){
     root = NULL;
 }
 
+
+int my_tree :: insert_root(node t){
+    if(root != NULL){
+        return 0;
+    }
+    node *p;
+    p = new node;
+    (*p) =t;
+    p ->left = NULL;
+    p ->right = NULL;
+    root = p;
+    node_count++;
+    return 1;
+}
+
+int my_tree :: insert_left(string tname, node tnode){
+    int result;
+    result = node_insert_left(root,tname,tnode);
+    if(result ==1){
+        node_count++;
+    }
+    return result;
+
+}
+
+int node_insert_left(node *p,string tname, node tnode){
+    if(p==NULL){
+        return 0;
+    }
+
+    if(p->name == tname){
+        if(p->left != NULL){
+            return -1;
+        }
+        node *t;
+        t = new node;
+        (*t) = tnode;
+        t->left = NULL;
+        t->right = NULL;
+        p->left =t;
+        return 1;
+    }
+    else{
+        int result;
+        result = node_insert_left(p->left, tname, tnode);
+        if(result != 0){
+            return result;
+        }
+        return node_insert_left(p->right, tname, tnode);
+        }
+    }
 
 
 int main(){
