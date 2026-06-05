ce9fb5fc31d757ae85329d60170c8b43eb4e03d3
HYC <yechanhwangahah@gmail.com>
2026-06-05 12:21:22 +0900

    함수 구현

--- PS/Binary_tree_PS/BT.cpp
+++ PS/Binary_tree_PS/BT.cpp
@@ -25,16 +25,42 @@ Node* makeTree(int *list ,int s){
     while(i<s){
         Node *cur = q.front();
         q.pop();
+        if(i<s){
+            cur ->left = new Node(list[i]);
+            q.push(cur->left);
+            i++;
+        }
+        if(i<s){
+            cur ->right = new Node(list[i]);
+            q.push(cur->right);
+            i++;
+        }
     }
-
-
     return root;
 }
 
-void clearTree(){}
-void inorderprint(){}
+void clearTree(Node *root){
+    if(root == nullptr){
+        return ;
+    }
+    clearTree(root ->left);
+    clearTree(root ->right);
+    delete root;
+}
+
+void inorderprint(Node *root){
+    if(root== nullptr){
+        return;
+    }
+    inorderprint(root ->left);
+    cout << root ->data << " ";
+    inorderprint(root ->right);
+}
 
 int main(){
-
+    int s;
+    cin >> s;
+    int *list = new int[s];
+    
     return 0;
 }
