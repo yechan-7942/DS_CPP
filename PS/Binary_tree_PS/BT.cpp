a46f66469c39b716d97597c0d7d3aa147467e53b
HYC <yechanhwangahah@gmail.com>
2026-06-05 12:01:04 +0900

    node 만들기

--- PS/Binary_tree_PS/BT.cpp
+++ PS/Binary_tree_PS/BT.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<queue>
+
 using namespace std;
 
 class Node{
@@ -10,7 +12,25 @@ public:
     Node(int v) : data(v), left(nullptr), right(nullptr) {}
 };
 
-void makeTree(){}
+Node* makeTree(int *list ,int s){
+    if(s ==0){
+        return nullptr;
+    }
+    queue <Node *> q;
+    Node *root = new Node(list[0]);
+
+    q.push(root);
+
+    int i = 1;
+    while(i<s){
+        Node *cur = q.front();
+        q.pop();
+    }
+
+
+    return root;
+}
+
 void clearTree(){}
 void inorderprint(){}
 
