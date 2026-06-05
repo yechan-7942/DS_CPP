616e4f66ca1595fb034986446a4adac8d4f03bf7
HYC <yechanhwangahah@gmail.com>
2026-06-05 11:46:48 +0900

    BT 기본 틀

---
+++ PS/Binary_tree_PS/BT.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int v) : data(v), left(nullptr), right(nullptr) {}
+};
+
+void makeTree(){}
+void clearTree(){}
+void inorderprint(){}
+
+int main(){
+
+    return 0;
+}
