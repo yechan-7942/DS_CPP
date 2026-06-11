e30c58527f129dba950cc5685b657d950eb073c3
HYC <yechanhwangahah@gmail.com>
2026-06-11 17:45:31 +0900

    inselction sort

---
+++ DS/Sort/sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+        if (minIdx != i)
+            swap(arr[i], arr[minIdx]);
+    }
+}
+
+void printArr(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Before: ";
+    printArr(arr, n);
+
+    selectionSort(arr, n);
+
+    cout << "After:  ";
+    printArr(arr, n);
+
+    return 0;
+}
